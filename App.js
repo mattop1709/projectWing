d2671e0372bf86e1e1303e6fc53dc5831deefa97
kamcoi <aimanadamx@gmail.com>
2017-10-11 18:42:29 +0800

    prior to datepicker

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Home from './src/components/home/Home'
 import Form from './src/components/requestor/Form'
+import Formv2 from './src/components/requestor/Formv2'
 import Main from './src/components/nominator/Main'
 import Task from './src/components/nominator/Task'
 import Request from './src/components/nominator/Request'
@@ -11,7 +12,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        <Form />
+        <Formv2 />
       </View>
     );
   }
---
+++ src/components/requestor/Formv2.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, View, KeyboardAvoidingView, ScrollView, TouchableOpacity } from 'react-native';
+
+import TravelForm from './TravelForm';
+import CostForm from './CostForm';
+import ApprovalForm from './ApprovalForm'
+
+class Formv2 extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = { page : 'travel'}
+  }
+  render() {
+    let displayedPage
+    switch (this.state.page) {
+      case 'travel':
+        displayedPage = <TravelForm />
+        break
+      case 'cost' :
+        displayedPage = <CostForm />
+        break
+      case 'approval' :
+        displayedPage = <ApprovalForm />
+        break
+      default:
+        displayedPage = <TravelForm />
+    }
+    return(
+      <View behavior='padding' style={{flex: 1, backgroundColor: '#c4c4c4'}}>
+        <View style={{height: 60, backgroundColor: 'grey', paddingTop: 32, alignItems: 'center'}}>
+          <Text style={{fontSize: 16}}>Application Form</Text>
+        </View>
+
+        <View style={{flex: 0.1, flexDirection: 'row', marginHorizontal: 16, marginVertical: 24}}>
+          <TouchableOpacity onPress={()=> this.setState ({ page: 'travel' })} style={{flex: 0.25, backgroundColor: 'yellow', justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderRightWidth: 0, borderTopLeftRadius: 8,  borderBottomLeftRadius: 8}}>
+            <Text style={{fontSize: 12}}>Step 1</Text>
+            <Text style={{fontSize: 12}}>Travel</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=> this.setState ({ page: 'cost' })} style={{flex: 0.25, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderWidth: 1}}>
+            <Text style={{fontSize: 12}}>Step 2</Text>
+            <Text style={{fontSize: 12}}>Cost</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=> this.setState ({ page: 'approval' })} style={{flex: 0.25, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderLeftWidth: 0}}>
+            <Text style={{fontSize: 12}}>Step 3</Text>
+            <Text style={{fontSize: 12}}>Approval</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=>null} style={{flex: 0.25, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderLeftWidth: 0, borderTopRightRadius: 8, borderBottomRightRadius: 8}}>
+            <Text style={{fontSize: 12}}>Step 4</Text>
+            <Text style={{fontSize: 12}}>Done!</Text>
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView style={{flex: 1}}>
+          <View style={{alignItems: 'center', paddingTop: 16}}>
+            {displayedPage}
+          </View>
+        </ScrollView>
+
+      </View>
+    )
+  }
+}
+
+export default Formv2
